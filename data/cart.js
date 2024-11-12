305ac1bd5a50ce638ba1da463581a50839c41bd2
HTGA75 <hg23750@gmail.com>
2024-11-12 10:16:57 +0530

    Make update quanitity work on checkout page from exercise

--- data/cart.js
+++ data/cart.js
@@ -80,3 +80,17 @@ export function resetCart() {
   cart = [];
   saveToStorage();
 }
+
+export function updateQuantity(productId, newQuantity) {
+  let matchingItem;
+
+  cart.forEach((cartItem) => {
+    if (productId === cartItem.productId) {
+      matchingItem = cartItem;
+    }
+  });
+
+  matchingItem.quantity = newQuantity;
+
+  saveToStorage();
+}
